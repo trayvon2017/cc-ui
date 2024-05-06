c1559091585b7f2a3022e38117a6030028045b5c
caofeibo- <690624138@qq.com>
2024-05-06 14:52:26 +0800

    update version

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+/*
+ * @Description:
+ * @Date: 2023-01-06 17:04:16
+ * @LastEditors: cfb
+ * @LastEditTime: 2023-07-26 20:23:45
+ * @FilePath: /von-ui/vue.config.js
+ */
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   pages: {
     index: {
-      entry: 'examples/main.js',
-      template: 'public/index.html',
-      filename: 'index.html'
-    }
+      entry: "examples/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+    },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.module
-      .rule('js')
-      .include
-        .add('/packages')
-        .end()
-      .use('babel')
-        .loader('babel-loader')
-  }
-})
+      .rule("js")
+      .include.add("/packages")
+      .end()
+      .use("babel")
+      .loader("babel-loader");
+  },
+});
